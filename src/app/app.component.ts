aa2685797636e58077d049608ad036e36ceedbe4
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-04-14 19:45:20 -0400

    Clase 237. Autenticación con Twitter y el logout del usuario.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 
-import { Observable } from 'rxjs';
+import { ChatService } from './services/chat.service';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +9,9 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  chats: Observable<any[]>;
 
-  constructor( db: AngularFirestore ) {
-    this.chats = db.collection('chats').valueChanges();
+  constructor(
+    public cs: ChatService
+  ) {
   }
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -35,10 +35,15 @@ export class ChatService {
   }
 
   login( proveedor: string ) {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    if ( proveedor === 'google') {
+      this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    } else {
+      this.afAuth.auth.signInWithPopup(new firebase.auth.TwitterAuthProvider());
+    }
   }
 
   logout() {
+    this.usuario = {};
     this.afAuth.auth.signOut();
   }
 
